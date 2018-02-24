0fdc7bb5a221a6a56c0a538d881bef1c0b9746fd
arteniioleg <oleg@artenii.email>
2018-02-24 20:23:05 +0200

    tests: create database in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,10 @@ $cmd = function ($cmd, $args = '') {
 };
 
 $cmd('cache:clear', '--no-warmup');
+$cmd('doctrine:database:create', '--quiet');
+$cmd('doctrine:query:sql', '\'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";\'');
 $cmd('doctrine:schema:drop', '--force');
 $cmd('doctrine:schema:create');
-$cmd('doctrine:query:sql', '\'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";\'');
 $cmd('doctrine:fixtures:load', '--no-interaction');
 
 require __DIR__.'/../vendor/autoload.php';

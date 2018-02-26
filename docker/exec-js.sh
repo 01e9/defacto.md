9e23fccbc50c9f1f9d0d34dbf2424e5fca11a74e
arteniioleg <oleg@artenii.email>
2018-02-26 20:20:07 +0200

    docker: npm-build.sh -> exec-js.sh

--- docker/exec-js.sh
+++ docker/exec-js.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run -it --rm -u ${UID} \
     -v ${SCRIPT_DIR}/..:/project \
     node:9 \
-    bash -c 'cd /project && npm install && npm run build'
+    bash -c "cd /project && $@"

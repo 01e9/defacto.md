4e012b506b67fa2100a2c6a9f425427028ccf660
arteniioleg <oleg@artenii.email>
2018-02-26 20:24:47 +0200

    made docker/exec*.sh script cd in project dir

--- docker/exec-js.sh
+++ docker/exec-js.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$(dirname ${SCRIPT_DIR})
 
 docker run -it --rm -u ${UID} \
-    -v ${SCRIPT_DIR}/..:/project \
+    -v ${PROJECT_DIR}:${PROJECT_DIR} \
     node:9 \
-    bash -c "cd /project && $@"
+    bash -c "cd ${PROJECT_DIR} && $@"
--- docker/exec.sh
+++ docker/exec.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker exec -it -u $UID defacto bash -c "$@"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$(dirname ${SCRIPT_DIR})
+
+docker exec -it -u $UID defacto bash -c "cd ${PROJECT_DIR} && $@"

01ca00872a962dd5e3d0a39569b83febe2a9255d
arteniioleg <oleg@artenii.email>
2018-03-10 17:43:59 +0200

    installed Algolia bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Algolia\SearchBundle\AlgoliaSearchBundle::class => ['all' => true],
 ];

6cc9fd1f34ae3dbba836b21ea8bf546085e74171
arteniioleg <oleg@artenii.email>
2018-03-11 15:43:51 +0200

    fixed: dismiss alert not working

--- assets/admin/js/index.js
+++ assets/admin/js/index.js
@@ -1,7 +1,8 @@
+import 'bootstrap';
 import 'froala-editor';
 import 'froala-editor/js/plugins/image.min';
 import 'froala-editor/js/plugins/code_view.min';
 
 jQuery(($) => {
     $('textarea.wysiwyg').froalaEditor();
-})
+});

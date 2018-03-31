e99d9204f843e99b68e99c53ba55070d0ce38009
arteniioleg <oleg@artenii.email>
2018-03-31 15:40:49 +0300

    created entity `Power`

---
+++ src/Entity/Power.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PowerRepository")
+ * @ORM\Table(
+ *     name="powers",
+ *     uniqueConstraints={
+ *      @ORM\UniqueConstraint(name="power_unique_slug", columns={"slug"})
+ *     }
+ * )
+ *
+ * @UniqueEntity(fields={"slug"})
+ */
+class Power
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=36)
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3, max=30)
+     * @Assert\Regex(
+     *     pattern="/^\p{L}+(\-\p{L}+)*$/u",
+     *     message="invalid.slug"
+     * )
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3, max=50)
+     */
+    private $name;
+
+    public function getId() : ?string
+    {
+        return $this->id;
+    }
+
+    public function getSlug() : ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug) : Power
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getName() : ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name) : Power
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}

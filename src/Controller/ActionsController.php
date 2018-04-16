225075592c6840b3a59139f8f7c392852e8caed6
arteniioleg <oleg@artenii.email>
2018-04-16 15:51:13 +0300

    begin action page

---
+++ src/Controller/ActionsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Action;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActionsController extends AbstractController
+{
+    /**
+     * @Route(path="/a/{id}", name="action")
+     * @Method("GET")
+     */
+    public function viewAction(Request $request, string $id)
+    {
+        /** @var Action $action */
+        $action = $this->getDoctrine()->getRepository('App:Action')->findOneBy([
+            'id' => $id,
+            'published' => true,
+        ]);
+
+        if (!$action) {
+            throw $this->createNotFoundException();
+        }
+
+        return new Response('Pagina acțiunei? Slug?');
+    }
+}

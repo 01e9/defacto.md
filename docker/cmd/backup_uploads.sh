d7fdaff6ba7a20fd7e5b72ef6b6893ff15ded5d2
arteniioleg <oleg@artenii.email>
2018-06-03 14:38:13 +0300

    docker: uploads backup

--- docker/cmd/backup_uploads.sh
+++ docker/cmd/backup_uploads.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-cp /backup/.pgpass ~/
-chmod 600 ~/.pgpass
-
 unset sleep
 trap 'echo TERMinated; kill $sleep; exit' TERM
 
 while : ; do
-    /backup/pg_backup.sh
+    /backup.sh /uploads /backups
     sleep 86400 & sleep=$$!; wait
 done

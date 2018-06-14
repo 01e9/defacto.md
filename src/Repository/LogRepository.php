36f4e5e30512a44915e64d6f1fdcefeaae23c30e
arteniioleg <oleg@artenii.email>
2018-06-14 22:45:02 +0300

    LogRepository: added methods to get Promise and Action logs

--- src/Repository/LogRepository.php
+++ src/Repository/LogRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\Action;
 use App\Entity\Log;
+use App\Entity\Promise;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -17,5 +19,23 @@ class LogRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Log::class);
+    }
+
+    public function findLatestByPromise(Promise $promise, $limit = 10)
+    {
+        return $this->findBy(
+            ['objectType' => 'promise', 'objectId' => $promise->getId()],
+            ['occurredTime' => 'desc'],
+            $limit
+        );
+    }
+
+    public function findLatestByAction(Action $action, $limit = 10)
+    {
+        return $this->findBy(
+            ['objectType' => 'action', 'objectId' => $action->getId()],
+            ['occurredTime' => 'desc'],
+            $limit
+        );
     }
 }

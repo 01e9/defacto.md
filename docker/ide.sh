912e1225e857305e9807c52ac708e2bb3629e42c
Oleg Artenii <oleg@artenii.email>
2018-12-01 23:47:53 +0200

    minor improvements in docker/ide.sh

--- docker/ide.sh
+++ docker/ide.sh
@@ -9,14 +9,13 @@ CMD=${@}
 
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 PROJECT_DIR=$(dirname ${SCRIPT_DIR})
-IMAGE=defacto_php
-NETWORK=defacto_default
+IMAGE='defacto_php'
+NETWORK='defacto_default'
+HOST='defacto.local'
 
-[ -n "$(docker images -q --filter=reference="${IMAGE}")" ] \
-    || docker build -t ${IMAGE} ${SCRIPT_DIR}
+docker build -t ${IMAGE} ${SCRIPT_DIR}
 
-[ -n "$(docker network ls -q --filter name=${NETWORK})" ] \
-    || docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up -d
+docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up -d
 
 x11docker \
     --hostdisplay \
@@ -25,9 +24,9 @@ x11docker \
     --stdout --stderr \
     --cap-default \
     --workdir ${PROJECT_DIR} \
-    --runasroot "apt-get update && apt-get install -y \
+    --runasroot "echo 'Installing GUI libraries...' && apt-get update && apt-get install -y \
         libgtk2.0-0 libcanberra-gtk-module libxext-dev libxrender-dev \
         libxtst-dev libxslt-dev dmz-cursor-theme \
         git wget htop zip unzip nano iputils-ping" \
-    -- "--cap-add=SYS_PTRACE --publish=80:8080 --network ${NETWORK}" \
+    -- "--cap-add=SYS_PTRACE --publish=80:8080 --network ${NETWORK} --hostname ${HOST}" \
     ${IMAGE} ${CMD}

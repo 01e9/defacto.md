39edb6cccdf9a54f96b203d956b03a4085338879
Oleg Artenii <oleg@artenii.email>
2018-12-24 15:15:00 +0200

    created Constituency entity

---
+++ src/Entity/Constituency.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ConstituencyRepository")
+ * @ORM\Table(
+ *     name="constituencies",
+ *     uniqueConstraints={
+ *      @ORM\UniqueConstraint(name="constituency_unique_slug", columns={"slug"})
+ *     }
+ * )
+ *
+ * @UniqueEntity(fields={"slug"})
+ */
+class Constituency
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="string", length=36)
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3, max=120)
+     * @Assert\Regex(
+     *     pattern="/^\p{L}+(\-\p{L}+)*$/u",
+     *     message="invalid.slug"
+     * )
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3, max=120)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Length(min=3)
+     * @Assert\Url()
+     */
+    private $link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSlug() : ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug) : self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}

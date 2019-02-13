9baff7645889a16c45e54ec4a1d4ac65df596089
Oleg Artenii <oleg@artenii.email>
2019-02-13 22:06:58 +0200

    localize gmaps js

--- assets/common/js/google-maps.ts
+++ assets/common/js/google-maps.ts
@@ -1,7 +1,13 @@
 import { includeScript } from "./utils";
 
 export function includeGoogleMapsScript(document: Document) {
-    const meta = document.querySelector("head > meta[name='google-maps-api-key']");
-    const apiKey = meta ? meta.getAttribute("content") : "";
-    return includeScript("https://maps.googleapis.com/maps/api/js?key=" + encodeURIComponent(apiKey), document);
+    const apiKeyMeta = document.querySelector("head > meta[name='google-maps-api-key']");
+    const apiKey = apiKeyMeta ? apiKeyMeta.getAttribute("content") : "";
+
+    const lang = document.querySelector('html').getAttribute('lang');
+
+    return includeScript(
+        "https://maps.googleapis.com/maps/api/js?key=" + encodeURIComponent(apiKey) + "&language=" + lang,
+        document
+    );
 }

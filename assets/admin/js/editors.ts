6432b8e048193af04994455f4cbefcfbf7347577
Oleg Artene <hi@01eg.art>
2019-06-16 18:11:39 +0200

    ckeditor: added heading

--- assets/admin/js/editors.ts
+++ assets/admin/js/editors.ts
@@ -4,7 +4,7 @@ import "@ckeditor/ckeditor5-build-classic/build/translations/ro"
 export function initElementEditors(element) {
     $(element).find('textarea.wysiwyg:not(.initialized)').addClass('initialized').each((i, textarea) => {
         ClassicEditor.create(textarea, {
-            toolbar: ['bold', 'italic', 'blockQuote', 'link', 'bulletedList', 'numberedList'],
+            toolbar: ['heading', '|', 'bold', 'italic', 'blockQuote', 'link', 'bulletedList', 'numberedList'],
             language: 'ro'
         });
     });

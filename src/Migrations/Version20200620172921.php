96352607614aecd3a4e0b2fc0238964b68d8c895
Oleg Artene <hi@01eg.art>
2020-06-20 20:34:07 +0300

    migration: disable presidential election isCompetenceUseTracked

---
+++ src/Migrations/Version20200620172921.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200620172921 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('UPDATE elections SET is_competence_use_tracked = \'false\' WHERE id = \'cd72c5bb-f110-4291-bbab-0ed28dbddad1\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('UPDATE elections SET is_competence_use_tracked = \'true\' WHERE id = \'cd72c5bb-f110-4291-bbab-0ed28dbddad1\'');
+    }
+}

1ce3e67ce1629aea7e22e887b6ad495533ea0f5a
Oleg Artene <hi@01eg.art>
2020-08-20 16:34:50 +0300

    text improvements

--- assets/app/js/index.ts
+++ assets/app/js/index.ts
@@ -6,6 +6,7 @@ import { initGoogleMapWithMarkers } from "./google-maps";
 import "bootstrap-select/js/bootstrap-select";
 
 $(() => $('[data-toggle="tooltip"]').tooltip());
+$(() => $('[data-toggle="popover"]').popover());
 
 $(() => $(".map-markers").each((i, element) => {
     !initGoogleMapWithMarkers(element, document, $(element).data("map-markers") || []);
